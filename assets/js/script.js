1cccc36da39431565d4dda0c3a2496cc5601930d
wangxiui <1036900394@qq.com>
2024-03-03 20:46:33 +0900

    update page style

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,20 @@ function handleUpload() {
   if (file) uploadImage(file);
 }
 
-function copyImageUrl() {
+async function copyImageUrl() {
   const imageUrl = document.getElementById('imageUrl');
-  imageUrl?.select();
-  document.execCommand('copy');
-  // navigator.clipboard.writeText(imageUrl.value)
+  try {
+    await navigator.clipboard.writeText(imageUrl.innerHTML);
+    document.getElementById('copy').style.display = "none"
+    document.getElementById('copied').style.display = "block"
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function recoverCopy() {
+  document.getElementById('copy').style.display = "block"
+  document.getElementById('copied').style.display = "none"
 }
 
 function uploadImage(file) {
@@ -42,19 +51,31 @@ function uploadImage(file) {
       const src = 'https://telegra.ph' + data[0].src;
       if(uploadStatus) {
         setUploadStatus(
-          `<div class="alert alert-success">Upload successful!</div>
-                <img src="${src}" class="img-fluid mb-3" alt="Uploaded Image">
-                <div class="input-group">
-                  <input type="text" class="form-control" id="imageUrl" value="${src}">
+          `<div class="mx-auto p-4 flex flex-wrap justify-around text-center">
+  <div class="relative w-52 h-60 img-card flex flex-col justify-center items-center items-cente border border-gray-200  rounded-lg shadow-md overflow-hidden mb-4 bg-white">
+ 
+  ${
+            file.type.startsWith("video")
+              ? `<video src="${ src }" class="img-fluid mb-3" controls></video>`
+              : `<img src="${ src }" class="w-full flex-1 min-h-0 flex justify-center items-center object-cover w-full h-full" alt="Uploaded Image">`
+          }
+                
+                <div class="w-full flex justify-center items-center space-x-1 px-2 py-1 animate-in slide-in-from-bottom">
+                  <span id="imageUrl" class="text-sm inline-block flex-1 truncate">${ src }</span>
                   <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="button" onclick="copyImageUrl()">Copy URL</button>
+                    <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 w-10 shrink-0" type="button" >
+                      <svg id="copy" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                           stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy " onclick="copyImageUrl()">
+                          <rect width="14" height="14" x="8" y="8" rx="2" ry="2"></rect>
+                          <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"></path>
+                      </svg>
+                      
+                      <svg id="copied" class="hidden" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" onclick="recoverCopy()"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" /></svg>
+                    </button>
                   </div>
                 </div>
-                ${
-                          file.type.startsWith("video")
-                            ? `<video src="${src}" class="img-fluid mb-3" controls></video>`
-                            : `<img src="${src}" class="img-fluid mb-3" alt="Uploaded Image">`
-                        }`
+  </div>
+</div>`
         )
       }
     })
